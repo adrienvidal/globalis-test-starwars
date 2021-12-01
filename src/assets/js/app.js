52b3978c102da8479f342e1aa4a671635601d5e7
Adrien Vidal <adriensg.vidal@gmail.com>
2021-12-01 22:04:02 +0100

    add parallax

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,38 @@
-const nav = document.querySelector('.nav');
-const navMenu = document.querySelector('.nav__menu');
+class App {
+  constructor() {
+    this.nav = document.querySelector('.nav');
+    this.navMenu = document.querySelector('.nav__menu');
+    this.scrolled = 0;
+  }
 
-navMenu.addEventListener('click', ()=>{
-  nav.classList.toggle('nav__active');
+  // init app
+  init () {
+    this.initNav()
+
+    window.addEventListener(
+      'scroll',
+      this.parallaxAnimation);
+  }
+
+  initNav() {    
+    this.navMenu.addEventListener('click', ()=>{
+      this.nav.classList.toggle('nav__active');
+    })
+  }
+  
+  parallaxAnimation() {
+    const parallaxElems = document.querySelectorAll(".parallax");
+    this.scrolled = window.pageYOffset;
+    parallaxElems.forEach((elem)=>{
+      const thisElem = elem;
+      const elemPosition = elem.offsetTop;
+      const coords = ((this.scrolled - elemPosition) * 0.4) + 'px'
+      thisElem.style.transform = 'translateY(' + coords + ')';
+    })
+  }
+}
+
+const app = new App();
+document.addEventListener('DOMContentLoaded', () => {
+  app.init()
 })
